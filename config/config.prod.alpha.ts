3dc0ea7c89d3b77c7f7179fcda6fa04d578d66ae
lwnmengjing <lwnmengjing@qq.com>
2023-12-07 11:26:25 +0800

    test

--- config/config.prod.alpha.ts
+++ config/config.prod.alpha.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
-  base: '/mss-boot-admin-antd/',
   define: {
     API_URL: 'https://admin-api-alpha.mss-boot-io.top',
   },
