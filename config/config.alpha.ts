2348ffc3560a6bc8df066711e80ef11ddb77c51f
lwnmengjing <lwnmengjing@qq.com>
2023-12-07 09:06:00 +0800

    add alpha env

--- config/config.alpha.ts
+++ config/config.alpha.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
   define: {
-    API_URL: 'https://admin-api-dev.mss-boot-io.top',
+    API_URL: 'https://admin-api-alpha.mss-boot-io.top',
   },
 });
